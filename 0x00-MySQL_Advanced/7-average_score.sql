463b73ad40b13101ca2f1121509819921b639ed4
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-07-13 12:20:48 +0000

    task 8

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,36 +1,14 @@
 -- Creates a stored procedure ComputeAverageScoreForUser that computes
 -- and store the average score for a student.
 
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-	DECLARE total_score FLOAT;
-	DECLARE count_scores INT;
 	DECLARE avg_score FLOAT;
-
-
-	-- Compute the total score for the user
-	SELECT SUM(score) INTO total_score
-	FROM corrections
-	WHERE user_id = user_id;
-
-	-- Count the number of scores for the user
-	SELECT COUNT(*) INTO count_scores
-	FROM corrections
-	WHERE user_id = user_id;
-
-	-- Compute the average score
-	IF count_scores > 0 THEN
-	    SET avg_score = total_score / count_scores;
-	ELSE
-	    SET avg_score = 0;
-	END IF;
-
-	-- Update the average_score column for the user
-	UPDATE users
-	SET average_score = avg_score
-	WHERE id = user_id;
-END //
-
+	SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+	UPDATE users SET average_score = avg_score WHERE id = user_id;
+END
+//
 DELIMITER ;
