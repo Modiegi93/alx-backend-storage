b9286c5ec2157d66cd4f275141f0c20215e138be
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-07-19 19:10:57 +0000

    task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,34 +2,37 @@
 """Get a web page"""
 import redis
 import requests
-from typing import Union
+from typing import Callable
+from functools import wraps
 
 
+r = redis.Redis()
+
+
+def count_requests(method: Callable) -> Callable:
+    """ Decorator for counting """
+    @wraps(method)
+    def wrapper(url):
+        """ Wrapper for decorator """
+        r.incr(f"count:{url}")
+        cached_html = r.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        r.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
+
+    
+@count_requests
 def get_page(url: str) -> str:
-    """Track a number of times a particular URL was accessed"""
-    r = redis.Redis()
+    """Get the html content of a url"""
+    req = requests.get(url)
+    return req.text
 
-    count_key = f"count:{url}"
-    cached_content_key = f"content:{url}"
-
-    count = r.get(count_key)
-    if count is None:
-        count = 1
-    else:
-        count = int(count) + 1
-
-    content = r.get(cached_content_key)
-    if content is None:
-        response = requests.get(url)
-        content = response.text
-
-        r.setex(cached_content_key, 10, content)
-
-    r.setex(count_key, 10, count)
-
-    return content
 
 if __name__ == "__main__":
-    url = ('http://slowwly.robertomurray.co.uk')
+    url = "http://slowwly.robertomurray.co.uk"
     page_content = get_page(url)
     print(page_content)
